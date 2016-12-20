5145d0f304155331a419e4a815e6fb8861a39929
William Galebach <willbach@gmail.com>
2016-12-20 18:52:52 +0000

    uploading to github

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json(), bodyParser.urlencoded());
+app.use(bodyParser.json(), bodyParser.urlencoded({extended: false}));
 
-app.post('/', (req, res) => {
+app.post('/test', (req, res) => {
+  console.log('we have a request');
   console.log(req.body);
   res.end()
-}
+});
 
-app.listen(3000, () => console.log('listening on port 3000));
+app.listen(3000, () => console.log('listening on port 3000'));
